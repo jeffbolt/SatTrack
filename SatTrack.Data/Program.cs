afa7f278ae3c269123d470dd261ca9a4f99ab1a2
jeffbolt <jeffbolt@users.noreply.github.com>
2021-07-21 15:15:17 -0500

    Changes to TleFormat. Added Line2 parsing.

--- SatTrack.Data/Program.cs
+++ SatTrack.Data/Program.cs
@@ -6,27 +6,34 @@ namespace SatTrack.Data
 	{
 		public static void Main()
 		{
-			var tle = new TleFormat
-			{
-				Line1 = "1 25544U 98067A   08264.51782528 -.00002182  00000-0 -11606-4 0  2927",
-				Line2 = "2 25544  51.6416 247.4627 0006703 130.5360 325.0288 15.72125391563537"
-			};
+			const string line1 = "1 25544U 98067A   08264.51782528 -.00002182  00000-0 -11606-4 0  2927";
+			const string line2 = "2 25544  51.6416 247.4627 0006703 130.5360 325.0288 15.72125391563537";
+			var tleFormat = new TleFormat(line1, line2);
 
-			var formatter = new TleFormatter(tle);
-			Console.WriteLine($"LineNumber: {formatter.LineNumber}");
-			Console.WriteLine($"SatelliteCatalogNumber: {formatter.SatelliteCatalogNumber}");
-			Console.WriteLine($"Classification: {formatter.Classification}");
-			Console.WriteLine($"CosparID LaunchYear: {formatter.CosparID.LaunchYear}");
-			Console.WriteLine($"CosparID LaunchNumber: {formatter.CosparID.LaunchNumber}");
-			Console.WriteLine($"CosparID LaunchPiece: {formatter.CosparID.LaunchPiece}");
-			Console.WriteLine($"EpochYear: {formatter.EpochYear}");
-			Console.WriteLine($"Epoch: {formatter.Epoch}");
-			Console.WriteLine($"MeanMotion Derivative 1: {formatter.MeanMotionD1}");
-			Console.WriteLine($"MeanMotion Derivative 2: {formatter.MeanMotionD2}");
-			Console.WriteLine($"Bstar: {formatter.Bstar}");
-			Console.WriteLine($"EphemerisType: {formatter.EphemerisType}");
-			Console.WriteLine($"ElementSetNumber: {formatter.ElementSetNumber}");
-			Console.WriteLine($"Checksum: {formatter.Checksum}");
+			Console.WriteLine($"Line 1 LineNumber: {tleFormat.TleLine1.LineNumber}");
+			Console.WriteLine($"Line 1 SatelliteCatalogNumber: {tleFormat.TleLine1.SatelliteCatalogNumber}");
+			Console.WriteLine($"Line 1 Classification: {tleFormat.TleLine1.Classification}");
+			Console.WriteLine($"Line 1 CosparID LaunchYear: {tleFormat.TleLine1.CosparID.LaunchYear}");
+			Console.WriteLine($"Line 1 CosparID LaunchNumber: {tleFormat.TleLine1.CosparID.LaunchNumber}");
+			Console.WriteLine($"Line 1 CosparID LaunchPiece: {tleFormat.TleLine1.CosparID.LaunchPiece}");
+			Console.WriteLine($"Line 1 EpochYear: {tleFormat.TleLine1.EpochYear}");
+			Console.WriteLine($"Line 1 Epoch: {tleFormat.TleLine1.Epoch}");
+			Console.WriteLine($"Line 1 MeanMotion Derivative 1: {tleFormat.TleLine1.MeanMotionD1}");
+			Console.WriteLine($"Line 1 MeanMotion Derivative 2: {tleFormat.TleLine1.MeanMotionD2}");
+			Console.WriteLine($"Line 1 Bstar: {tleFormat.TleLine1.Bstar}");
+			Console.WriteLine($"Line 1 EphemerisType: {tleFormat.TleLine1.EphemerisType}");
+			Console.WriteLine($"Line 1 ElementSetNumber: {tleFormat.TleLine1.ElementSetNumber}");
+			Console.WriteLine($"Line 1 Checksum: {tleFormat.TleLine1.Checksum}");
+
+			Console.WriteLine($"Line 2 LineNumber: {tleFormat.TleLine2.LineNumber}");
+			Console.WriteLine($"Line 2 SatelliteCatalogNumber: {tleFormat.TleLine2.SatelliteCatalogNumber}");
+			Console.WriteLine($"Line 2 Inclination: {tleFormat.TleLine2.Inclination}");
+			Console.WriteLine($"Line 2 RightAscensionOfAscendingNode: {tleFormat.TleLine2.RightAscensionOfAscendingNode}");
+			Console.WriteLine($"Line 2 Eccentricity: {tleFormat.TleLine2.Eccentricity}");
+			Console.WriteLine($"Line 2 ArgumentOfPerigee: {tleFormat.TleLine2.ArgumentOfPerigee}");
+			Console.WriteLine($"Line 2 MeanAnomaly: {tleFormat.TleLine2.MeanAnomaly}");
+			Console.WriteLine($"Line 2 MeanMotion: {tleFormat.TleLine2.MeanMotion}");
+			Console.WriteLine($"Line 2 Checksum: {tleFormat.TleLine2.Checksum}");
 		}
 	}
 }
--- SatTrack.Data/TleFormat.cs
+++ SatTrack.Data/TleFormat.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 
@@ -8,26 +7,55 @@ namespace SatTrack.Data
 	// https://en.wikipedia.org/wiki/Two-line_element_set
 	public class TleFormat
 	{
-		[MaxLength(69)]
-		public string Line1 { get; set; }
-		[MaxLength(69)]
-		public string Line2 { get; set; }
+		//[MaxLength(69)]
+		//private string line1 { get; set; }
+		//[MaxLength(69)]
+		//private string line2 { get; set; }
+		public TleLine1 TleLine1 { get; }
+		public TleLine2 TleLine2 { get; }
+
+		public TleFormat(string line1, string line2)
+		{
+
+			TleLine1 = new TleLine1
+			{
+				LineNumber = ushort.Parse(line1.Substring(0, 1)),
+				SatelliteCatalogNumber = ushort.Parse(line1.Substring(2, 5)),
+				Classification = line1.ElementAt(7),
+				CosparID = new InternationalDesignator
+				{
+					LaunchYear = ushort.Parse(line1.Substring(9, 2)),
+					LaunchNumber = ushort.Parse(line1.Substring(11, 3)),
+					LaunchPiece = line1.Substring(14, 3).Trim()
+				},
+				EpochYear = ushort.Parse(line1.Substring(18, 2).Trim()),
+				Epoch = double.Parse(line1.Substring(20, 11).Trim()),
+				MeanMotionD1 = double.Parse(line1.Substring(33, 10).Trim()),
+				MeanMotionD2 = double.Parse(line1.Substring(44, 6).Trim()) * Math.Pow(10, double.Parse(line1.Substring(51, 1).Trim())),
+				Bstar = double.Parse(line1.Substring(53, 5).Trim()) * Math.Pow(10, double.Parse(line1.Substring(60, 1).Trim())),
+				EphemerisType = ushort.Parse(line1.Substring(62, 1).Trim()),
+				ElementSetNumber = ushort.Parse(line1.Substring(65, 3).Trim()),
+				Checksum = ushort.Parse(line1.Substring(68, 1).Trim())
+			};
+
+			TleLine2 = new TleLine2
+			{
+				LineNumber = ushort.Parse(line2.Substring(0, 1)),
+				SatelliteCatalogNumber = ushort.Parse(line2.Substring(2, 6)),
+				Inclination = double.Parse(line2.Substring(8, 8).Trim()),
+				RightAscensionOfAscendingNode = double.Parse(line2.Substring(17, 8).Trim()),
+				Eccentricity = double.Parse(line2.Substring(26, 7).Trim()),
+				ArgumentOfPerigee = double.Parse(line2.Substring(34, 8).Trim()),
+				MeanAnomaly = double.Parse(line2.Substring(43, 7).Trim()),
+				MeanMotion = double.Parse(line2.Substring(52, 11).Trim()),
+				RevolutionNumberAtEpoch = ulong.Parse(line2.Substring(63, 5).Trim()),
+				Checksum = ushort.Parse(line2.Substring(68, 1).Trim())
+			};
+		}
 	}
 
-	public class InternationalDesignator
+	public class TleLine1
 	{
-		// aka COSPAR ID https://en.wikipedia.org/wiki/International_Designator
-		public ushort LaunchYear { get; set; }      // Field: 4, Columns: 10–11	(last two digits of launch year)
-		public ushort LaunchNumber { get; set; }    // Field: 5, Columns: 12–14 (launch number of the year)
-		[MaxLength(3)]
-		public string LaunchPiece { get; set; }     // Field: 6, Columns: 15–17 (piece of the launch)
-	}
-
-	public class TleFormatter
-	{
-		private readonly TleFormat _tle;
-
-		#region Line 1 Properties
 		public ushort LineNumber { get; set; }                  // Field: 1, Columns: 01–01
 		public ushort SatelliteCatalogNumber { get; set; }      // Field: 2, Columns: 03–07 https://en.wikipedia.org/wiki/Satellite_Catalog_Number
 		public char Classification { get; set; }                // Field: 3, Columns: 08–08 (U=Unclassified, C=Classified, S=Secret)
@@ -40,34 +68,67 @@ namespace SatTrack.Data
 		public ushort EphemerisType { get; set; }               // Field: 12, Columns: 63–63 (internal use only - always zero in distributed TLE data)
 		public ushort ElementSetNumber { get; set; }            // Field: 13, Columns: 65–68 (Incremented when a new TLE is generated for this object)
 		public ushort Checksum { get; set; }                    // Field: 14, Columns: 69–69 (modulo 10)
-		#endregion
+	}
 
-		#region Line 2 Properties
+	public class TleLine2
+	{
+		public ushort LineNumber { get; set; }                  // Field: 1, Columns: 01–01
+		public ushort SatelliteCatalogNumber { get; set; }      // Field: 2, Columns: 03–07 https://en.wikipedia.org/wiki/Satellite_Catalog_Number
+		public double Inclination { get; set; }                 // Field: 3, Columns: 09–16 (degrees) https://en.wikipedia.org/wiki/Orbital_inclination
+		public double RightAscensionOfAscendingNode { get; set; } // Field: 4, Columns: 18–25 (degrees) https://en.wikipedia.org/wiki/Right_ascension_of_the_ascending_node
+		public double Eccentricity { get; set; }                // Field: 5, Columns: 27–33 (decimal point assumed) https://en.wikipedia.org/wiki/Orbital_eccentricity
+		public double ArgumentOfPerigee { get; set; }           // Field: 6, Columns: 35–42 (degrees) https://en.wikipedia.org/wiki/Argument_of_perigee
+		public double MeanAnomaly { get; set; }                 // Field: 7, Columns: 44–51 (degrees) https://en.wikipedia.org/wiki/Mean_Anomaly
+		public double MeanMotion { get; set; }                  // Field: 7, Columns: 53–63 (revolutions per day) https://en.wikipedia.org/wiki/Mean_Motion
+		public ulong RevolutionNumberAtEpoch { get; set; }      // Field: 8, Columns: 64–68 (revolutions) 
+		public ushort Checksum { get; set; }                    // Field: 9, Columns: 69–69 (modulo 10)
+	}
 
-		#endregion
+	public class InternationalDesignator
+	{
+		// aka COSPAR ID https://en.wikipedia.org/wiki/International_Designator
+		public ushort LaunchYear { get; set; }                  // Field: 4, Columns: 10–11	(last two digits of launch year)
+		public ushort LaunchNumber { get; set; }                // Field: 5, Columns: 12–14 (launch number of the year)
+		[MaxLength(3)]
+		public string LaunchPiece { get; set; }                 // Field: 6, Columns: 15–17 (piece of the launch)
+	}
 
-		public TleFormatter(TleFormat tle)
-		{
-			_tle = tle;
+	//public class TleFormatter
+	//{
+	//	public TleFormat TleFormat { get; set; }
 
-			LineNumber = ushort.Parse(_tle.Line1.Substring(0, 1));
-			SatelliteCatalogNumber = ushort.Parse(_tle.Line1.Substring(2, 5));
-			Classification = _tle.Line1.ElementAt(7);
-			CosparID = new InternationalDesignator
-			{
-				LaunchYear = ushort.Parse(_tle.Line1.Substring(9, 2)),
-				LaunchNumber = ushort.Parse(_tle.Line1.Substring(11, 3)),
-				LaunchPiece = _tle.Line1.Substring(14, 3).Trim()
-			};
-			EpochYear = ushort.Parse(_tle.Line1.Substring(18, 2).Trim());
-			Epoch = double.Parse(_tle.Line1.Substring(20, 11).Trim());
-			MeanMotionD1 = double.Parse(_tle.Line1.Substring(33, 10).Trim());
-			MeanMotionD2 = double.Parse(_tle.Line1.Substring(44, 6).Trim()) * Math.Pow(10, double.Parse(_tle.Line1.Substring(51, 1).Trim()));
-			Bstar = double.Parse(_tle.Line1.Substring(53, 5).Trim()) * Math.Pow(10, double.Parse(_tle.Line1.Substring(60, 1).Trim()));
-			EphemerisType = ushort.Parse(_tle.Line1.Substring(62, 1).Trim());
-			ElementSetNumber = ushort.Parse(_tle.Line1.Substring(65, 3).Trim());
-			Checksum = ushort.Parse(_tle.Line1.Substring(68, 1).Trim());
-		}
+	//	public TleFormatter(TleFormat tleFormat)
+	//	{
+	//// Line 1
+	//TleLine1.LineNumber = ushort.Parse(Line1.Substring(0, 1));
+	//TleLine1.SatelliteCatalogNumber = ushort.Parse(Line1.Substring(2, 5));
+	//TleLine1.Classification = Line1.ElementAt(7);
+	//TleLine1.CosparID = new InternationalDesignator
+	//{
+	//	LaunchYear = ushort.Parse(Line1.Substring(9, 2)),
+	//	LaunchNumber = ushort.Parse(Line1.Substring(11, 3)),
+	//	LaunchPiece = Line1.Substring(14, 3).Trim()
+	//};
+	//TleLine1.EpochYear = ushort.Parse(Line1.Substring(18, 2).Trim());
+	//TleLine1.Epoch = double.Parse(Line1.Substring(20, 11).Trim());
+	//TleLine1.MeanMotionD1 = double.Parse(Line1.Substring(33, 10).Trim());
+	//TleLine1.MeanMotionD2 = double.Parse(Line1.Substring(44, 6).Trim()) * Math.Pow(10, double.Parse(Line1.Substring(51, 1).Trim()));
+	//TleLine1.Bstar = double.Parse(Line1.Substring(53, 5).Trim()) * Math.Pow(10, double.Parse(Line1.Substring(60, 1).Trim()));
+	//TleLine1.EphemerisType = ushort.Parse(Line1.Substring(62, 1).Trim());
+	//TleLine1.ElementSetNumber = ushort.Parse(Line1.Substring(65, 3).Trim());
+	//TleLine1.Checksum = ushort.Parse(Line1.Substring(68, 1).Trim());
+
+	//// Line 2
+	//TleLine2.LineNumber = ushort.Parse(Line2.Substring(0, 1));
+	//TleLine2.SatelliteCatalogNumber = ushort.Parse(Line2.Substring(2, 6));
+	//TleLine2.Inclination = double.Parse(Line2.Substring(8, 6).Trim());
+	//TleLine2.RightAscensionOfAscendingNode = double.Parse(Line2.Substring(17, 8).Trim());
+	//TleLine2.Eccentricity = double.Parse(Line2.Substring(34, 8).Trim());
+	//TleLine2.ArgumentOfPerigee = double.Parse(Line2.Substring(34, 8).Trim());
+	//TleLine2.MeanAnomaly = double.Parse(Line2.Substring(43, 7).Trim());
+	//TleLine2.MeanMotion = double.Parse(Line2.Substring(52, 11).Trim());
+	//TleLine1.Checksum = ushort.Parse(Line1.Substring(68, 1).Trim());
+}
 
 		//public TleFormatter(TleFormat tle)
 		//{
@@ -108,5 +169,4 @@ namespace SatTrack.Data
 		//	//https://www.c-sharpcorner.com/uploadfile/40e97e/generic-method-for-parsing-value-type-in-C-Sharp/
 		//	return typeof(T).Parse<T>(string.Concat(list.GetRange(start, length)).Trim());
 		//}
-	}
-}
+	//}
